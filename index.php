c5ce73039173ecb25c95b354e4b4b55acd7a1bf1
Karla Calderón <51063773+karlacalderon@users.noreply.github.com>
2020-03-02 12:17:36 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html amp lang="en">
+  <head>
+    <meta charset="utf-8">
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <title>Karla Calderón - Diseñadora digital</title>
+    <link rel="canonical" href="https://amp.dev/es/documentation/guides-and-tutorials/start/create/basic_markup/">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <script async custom-element="amp-fx-collection" src="https://cdn.ampproject.org/v0/amp-fx-collection-0.1.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800&display=swap" rel="stylesheet">
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+      <style amp-custom>
+          *{
+              margin: 0;
+              padding: 0;
+              font-family: 'Open Sans';
+          }
+          a{
+              text-decoration: none;
+              color: #102E4A;
+          }
+  body {
+    background-color: #E8F1F2;
+  }
+          h1{
+              margin-top: 50px;
+              font-size: 5vh;
+              color: #102E4A;
+               text-decoration: underline;
+  text-decoration-color: #56E39F;
+          }
+               
+  
+          li{
+              list-style: none;
+          }
+
+          .center{
+              margin: 0 auto;
+              width: 90%;
+              max-width: 600px;
+          }
+          .header-links{
+              display: flex;
+              margin-top: 50px;
+              margin-bottom: 50px;
+              justify-content:flex-start;
+          }
+          .header-links li{
+              list-style: none;
+              display: inline-block;
+              font-weight: 800;
+              padding-right: 30px;
+              
+              
+          }
+          .header-links li a:hover{
+           border-bottom: 2px solid  #56E39F;
+         
+          }
+          h2{
+                margin-top: 50px;
+              margin-bottom: 20px;
+              font-size: 3vh;
+              color: #102E4A;
+               text-decoration: underline;
+  text-decoration-color: #56E39F;
+          }
+          
+        .bold li{
+              font-size: 21px;
+          }
+          footer{
+              background-color: #102E4A;
+              width: 100%;
+              height:150px;
+              margin-top: 50px;
+          }
+</style>
+  </head>
+  <body>
+      <div class="center">
+    <h1 amp-fx="fade-in">Hola 🖖 <br>
+    soy Karla Calderón<br>
+      diseñadora web y <br>
+        exploradora digital</h1>
+      <ul class="header-links">
+    <li><a href="mailto:karla10calderon@gmail.com?Subject=FROM%20KARLAWEB">Contáctame</a></li>
+          
+     <li><a href="https://github.com/karlacalderon" target="_blank">Github</a></li>      
+    </ul>
+      </div>
+      <div class="center">
+      <p>Soy egresada de la licenciatura en diseño gráfico por la UABC, interesada en el mundo digital. Desde hace unos decidí emprender en internet con mi proyecto Rock and Vegan.com, un sitio que me permite desarrollar diferentes habilidades todo el tiempo. Actualmente coloboro de manera freelance con diferentes emprendedores y proyectos de todo tipo.</p>
+      </div>
+      <div class="center">
+      <h2>Habilidades</h2>
+      <ul class="bold">
+      <li>HTML/ CSS</li>      
+      <li>Desarrollo en Wordpress</li>      
+      <li>SEO técnico</li>           
+      <li>E-commerce</li>           
+      <li>Google AMP</li>           
+            
+      </ul>
+      </div>
+      
+      <div class="center">
+      <h2>Cosas que estoy aprendiendo</h2>
+      <ul class="bold">
+      <li>UX / UI</li>
+      <li>CSS animations</li>
+       <li>React.js</li>  
+      <li>Javascript</li>      
+        
+      </ul>
+      </div>
+      
+       <div class="center">
+      <h2>Proyectos y colaboraciones recientes</h2>
+      <ul class="bold">
+      <li>Rock and Vegan</li>
+     <li>Limarp Clinic</li>
+      <li>San Diego Film Week</li>  
+      <li>San Diego Film Consortium</li>      
+      
+              
+      </ul>
+      </div>
+      
+        <div class="center">
+      <h2>Proyectos y colaboraciones recientes</h2>
+      <ul class="bold">
+      <li>Rock and Vegan</li>
+     <li>Limarp Clinic</li>
+      <li>San Diego Film Week</li>  
+      <li>San Diego Film Consortium</li>      
+      
+              
+      </ul>
+      </div>
+      
+      <footer>
+      </footer>
+  </body>
+</html>
